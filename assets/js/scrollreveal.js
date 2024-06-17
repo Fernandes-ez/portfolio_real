9b42d9574b2594f607d89ac7a0939d33a7abf4b8
Fernandes-ez <95105000+Fernandes-ez@users.noreply.github.com>
2024-06-17 18:12:45 -0300

    redsign e alterações mobile

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal({reset: true});
-ScrollReveal().reveal('.sobre_mim')
-ScrollReveal().reveal('.habilidades', {delay: 500})
-ScrollReveal().reveal('.projetos',{delay: 500})
-ScrollReveal().reveal('.contato',{delay: 500})
+ScrollReveal().reveal('.sobre_mim'), {delay: 600}
+ScrollReveal().reveal('.habilidades', {delay: 600})
+ScrollReveal().reveal('.projetos',{delay: 600})
+ScrollReveal().reveal('.contato',{delay: 600})
